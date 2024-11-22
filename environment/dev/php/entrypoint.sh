729fc909b8fcad8f9494328ab3a70c8b2d884e7d
Sebastian Górski <sebastiangorski00@gmail.com>
2024-11-22 14:59:36 +0100

    save

---
+++ environment/dev/php/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
+
+if [ ! -f ".env" ]; then
+    cp .env.example .env
+fi
+
+php artisan migrate
+php artisan key:generate
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+
+php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+exec docker-php-entrypoint "$@"
